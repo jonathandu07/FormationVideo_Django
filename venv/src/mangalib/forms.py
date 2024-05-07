eef4d2838aa3c33c01d0df641d5c1654e533c4c3
jonathan YHWH <alphajorider@gmail.com>
2024-05-07 13:27:56 +0200

    BookForm

--- venv/src/mangalib/forms.py
+++ venv/src/mangalib/forms.py
@@ -1,2 +1,10 @@
 from django import forms
+from .models import Author, Book
 
+class BookForm(forms.ModelForm):
+    author = forms.ModelChoiceField(queryset=Author.objects.all(),label="Auteur")
+    
+    class Meta:
+        model = Book
+        fields = ['tile', 'author', 'quantity']
+        lables = {'titles': 'Titre', 'quantity': 'Quantité'}
