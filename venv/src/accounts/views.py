5dde6ef69bc0140a026f5cea254eb5e156e6a17c
jonathan YHWH <alphajorider@gmail.com>
2024-05-08 18:41:02 +0200

    login page

--- venv/src/accounts/views.py
+++ venv/src/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST["password"]
+        user = authenticate(request, username = username, password = password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect("mangalib:index")
+        
+        else:
+            message.info(request, "Identifiant ou mot de passe incorrecte")
+
+    form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form":form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("mangalib:index")
+
+def register_user(request):
+    pass
