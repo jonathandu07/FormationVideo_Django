89f0e3c4de1a1ef35d9d398b1539bc7c5d6d3f00
jonathan YHWH <alphajorider@gmail.com>
2024-05-09 20:36:34 +0200

    faire un test

--- venv/src/mangalib/tests.py
+++ venv/src/mangalib/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Author
 
 # Create your tests here.
+class AuthorTestCase(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(name="Athanase Alcide")
+    
+    def test_iscorrect_instance(self):
+        self.assertIsInstance(self.author, Author)
